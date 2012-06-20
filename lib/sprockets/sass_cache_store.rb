ecdee2a6694bdbad0a17c5e0011c10733ccea098
Markus Schirp <mbj@seonic.net>
2012-06-20 13:40:01 +0200

    Add Sprockets::SassCacheStore#path_to(key)
    This method might be called when the sass base cache store resuces an
    exception to generate a warning from.

--- lib/sprockets/sass_cache_store.rb
+++ lib/sprockets/sass_cache_store.rb
@@ -9,17 +9,22 @@ module Sprockets
     end
 
     def _store(key, version, sha, contents)
-      environment.cache_set("sass/#{key}", {:version => version, :sha => sha, :contents => contents})
+      environment.cache_set(path_to(key), {:version => version, :sha => sha, :contents => contents})
     end
 
     def _retrieve(key, version, sha)
-      if obj = environment.cache_get("sass/#{key}")
+      if obj = environment.cache_get(path_to(key))
         return unless obj[:version] == version
         return unless obj[:sha] == sha
         obj[:obj]
       else
         nil
       end
+    end
+
+    def path_to(key)
+      raise
+      "sass/#{key}"
     end
   end
 end
